46110e9e4838c56cbbd88715a3dde30d7e176ecf
pdd <plsof@pdds-MacBook-Pro.local>
2019-09-10 11:16:28 +0800

    去掉用户登陆时对用户名的检查

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -74,8 +74,8 @@ export default {
     }
     return {
       loginForm: {
-        username: 'admin',
-        password: 'ysten123'
+        username: '',
+        password: ''
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
