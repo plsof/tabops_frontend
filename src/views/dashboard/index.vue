d699cf7d645aa3982e759b5738f39e0600837130
pdd <plsof@promote.cache-dns.local>
2019-09-14 22:07:32 +0800

    dashboard用户访问权限修改

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,30 +1,14 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ username }}</div>
+    <admin-dashboard />
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import adminDashboard from './admin'
 
 export default {
   name: 'Dashboard',
-  computed: {
-    ...mapGetters([
-      'username'
-    ])
-  }
+  components: { adminDashboard }
 }
 </script>
-
-<style lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
-  }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
-}
-</style>
