9225c2ace1f07298f0a88de8584d0188c4cf889d
plsof <1103901630@qq.com>
2019-09-30 14:41:48 +0800

    添加architecture-ubs

---
+++ src/api/architecture/ubs.js
@@ -0,0 +1,32 @@
+import request from '@/utils/request'
+
+export function fetchUBS(query) {
+  return request({
+    url: '/architecture/ubs',
+    method: 'get',
+    params: query
+  })
+}
+
+export function createUBS(data) {
+  return request({
+    url: '/architecture/ubs/',
+    method: 'post',
+    data
+  })
+}
+
+export function updateUBS(data) {
+  return request({
+    url: '/architecture/ubs/' + data.id,
+    method: 'put',
+    data
+  })
+}
+
+export function deleteUBS(id) {
+  return request({
+    url: '/architecture/ubs/' + id,
+    method: 'delete'
+  })
+}
