8a45eadd5a9a63d38b062013979ecf5dbcbd8ccd
plsof <1103901630@qq.com>
2019-09-30 14:40:10 +0800

    添加architecture-mgs

---
+++ src/api/architecture/mgs.js
@@ -0,0 +1,32 @@
+import request from '@/utils/request'
+
+export function fetchMGS(query) {
+  return request({
+    url: '/architecture/mgs',
+    method: 'get',
+    params: query
+  })
+}
+
+export function createMGS(data) {
+  return request({
+    url: '/architecture/mgs/',
+    method: 'post',
+    data
+  })
+}
+
+export function updateMGS(data) {
+  return request({
+    url: '/architecture/mgs/' + data.id,
+    method: 'put',
+    data
+  })
+}
+
+export function deleteMGS(id) {
+  return request({
+    url: '/architecture/mgs/' + id,
+    method: 'delete'
+  })
+}
